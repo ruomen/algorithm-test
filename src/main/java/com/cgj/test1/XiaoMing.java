c1db940f0122fcc7e372e77cece7df5d43318a79
‭‭若梦 <1823636424@qq.com>
2022-07-29 11:36:28 +0800

    创建 XiaoMing 类

---
+++ src/main/java/com/cgj/test1/XiaoMing.java
@@ -0,0 +1,14 @@
+package com.cgj.test1;
+
+/**
+ * @Description TODO
+ * @Author CGJ
+ * @Date 2022/7/29 11:35
+ * @Version 1.0
+ **/
+public class XiaoMing {
+
+    public static void main(String[] args) {
+        System.out.println("git hello");
+    }
+}
